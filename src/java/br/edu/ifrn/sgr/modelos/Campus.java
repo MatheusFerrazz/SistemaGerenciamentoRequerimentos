6c436f1842629c86860685cc2687cc8f40983331
CarlosHenriquePires <carluxhenrique@gmail.com>
2016-03-09 16:34:56 -0300

    criação do construtor vazio.

--- src/java/br/edu/ifrn/sgr/modelos/Campus.java
+++ src/java/br/edu/ifrn/sgr/modelos/Campus.java
@@ -33,7 +33,7 @@ public class Campus {
     }
 
     public Campus() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     /**
