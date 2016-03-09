638539b3d9f04a69bebfa09e5ea6ef2c71586ec6
CarlosHenriquePires <carluxhenrique@gmail.com>
2016-03-09 15:28:13 -0300

    Instancia de objetos em AlunoDAO

--- src/java/br/edu/ifrn/sgr/persistencias/AlunoDAO.java
+++ src/java/br/edu/ifrn/sgr/persistencias/AlunoDAO.java
@@ -6,6 +6,12 @@
 package br.edu.ifrn.sgr.persistencias;
 
 import br.edu.ifrn.sgr.modelos.Aluno;
+import br.edu.ifrn.sgr.modelos.Campus;
+import br.edu.ifrn.sgr.modelos.Coordenador;
+import br.edu.ifrn.sgr.modelos.Curso;
+import br.edu.ifrn.sgr.modelos.ModalidadeCurso;
+import br.edu.ifrn.sgr.modelos.Turma;
+import br.edu.ifrn.sgr.modelos.Turno;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -48,6 +54,36 @@ public class AlunoDAO extends GeralDAO {
         java.sql.Date dataSQL = resultado.getDate("datanascimento");
         java.util.Date data = new java.util.Date(dataSQL.getTime());
         aluno.setDataNascimento(data);
+        
+         
+        Curso curso = new Curso();
+        curso.setAnos(resultado.getInt("ano"));
+        curso.setNome(resultado.getString("nomeCurso"));
+        curso.setPeriodo(resultado.getInt("periodo"));
+        curso.setCursoAtivo(resultado.getBoolean("cursoAtivo"));
+        
+        ModalidadeCurso modalidadeCurso = new ModalidadeCurso();
+        modalidadeCurso.setModalidadeID(resultado.getInt("modalidadeCurso"));
+        modalidadeCurso.setNivel(resultado.getString("modalidadeCurso"));
+        modalidadeCurso.setNome(resultado.getString("nomeModalidade"));
+                
+        Campus campus = new Campus();
+        campus.setCampusID(resultado.getInt("idCampus"));
+        campus.setNome(resultado.getString("nomeCampus"));
+        campus.setEndereco(resultado.getString("campusEndereco"));
+        campus.setBairro(resultado.getString("campusBairro"));
+        campus.setEstado(resultado.getString("campusEstado"));
+        campus.setCep(resultado.getString("campusCep"));
+        campus.setTelefone(resultado.getString("campusTelefone"));
+        
+        Turno turno = new Turno();
+        turno.setId(resultado.getInt("idTurno"));
+        turno.setNome(resultado.getString("nomeTurno"));
+              
+        Coordenador coordenador = new Coordenador();
+                 
+        Turma turma = new Turma();
+        turma.setCodigo(resultado.getInt("codigoTurma"));
         
         return aluno;
     }
