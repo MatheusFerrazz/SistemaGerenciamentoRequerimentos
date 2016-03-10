a3614cf317841004652efc5f1c6bfa970aa7c1e4
CarlosHenriquePires <carluxhenrique@gmail.com>
2016-03-10 13:55:18 -0300

    Encapsulado objetos na classe AlunoDAO

--- src/java/br/edu/ifrn/sgr/persistencias/AlunoDAO.java
+++ src/java/br/edu/ifrn/sgr/persistencias/AlunoDAO.java
@@ -13,11 +13,6 @@ import br.edu.ifrn.sgr.modelos.Disciplina;
 import br.edu.ifrn.sgr.modelos.Ifrn;
 import br.edu.ifrn.sgr.modelos.ModalidadeCurso;
 import br.edu.ifrn.sgr.modelos.Permissao;
-import br.edu.ifrn.sgr.modelos.Professor;
-import br.edu.ifrn.sgr.modelos.TecnicoAdministrativo;
-import br.edu.ifrn.sgr.modelos.TipoAtividade;
-import br.edu.ifrn.sgr.modelos.TipoDocumento;
-import br.edu.ifrn.sgr.modelos.TipoRequerimento;
 import br.edu.ifrn.sgr.modelos.Turma;
 import br.edu.ifrn.sgr.modelos.Turno;
 import java.sql.ResultSet;
@@ -51,7 +46,22 @@ public class AlunoDAO extends GeralDAO {
 //    }
     
     public Aluno popularAluno(ResultSet resultado) throws SQLException{
+        
+        
         Aluno aluno = new Aluno();
+        Campus campus = new Campus();
+        Coordenador coordenador = new Coordenador();
+        Curso curso = new Curso();
+        Diretor diretor = new Diretor();
+        Disciplina disciplina = new Disciplina();
+        Ifrn ifrn = new Ifrn();
+        ModalidadeCurso modalidadeCurso = new ModalidadeCurso();
+        Permissao permissao = new Permissao();
+        Turma turma = new Turma();
+        Turno turno = new Turno();
+        
+        
+        
         aluno.setNome(resultado.getString("nomeAluno"));
         aluno.setMatricula(resultado.getString("matriculaAluno"));
         aluno.setEmail(resultado.getString("emailAluno"));
@@ -61,7 +71,8 @@ public class AlunoDAO extends GeralDAO {
         java.util.Date data = new java.util.Date(dataAlunoSQL.getTime());
         aluno.setDataNascimento(data);
         
-        Campus campus = new Campus();
+        
+        
         campus.setCampusID(resultado.getInt("idCampus"));
         campus.setNome(resultado.getString("nomeCampus"));
         campus.setEndereco(resultado.getString("campusEndereco"));
@@ -70,7 +81,7 @@ public class AlunoDAO extends GeralDAO {
         campus.setCep(resultado.getString("campusCep"));
         campus.setTelefone(resultado.getString("campusTelefone"));
         
-        Coordenador coordenador = new Coordenador();
+        
         coordenador.setNome(resultado.getString("nomeCoordenador"));
         coordenador.setMatricula(resultado.getString("matriculaCoordenador"));
         coordenador.setEmail(resultado.getString("emailCoordenador"));
@@ -80,14 +91,14 @@ public class AlunoDAO extends GeralDAO {
         java.util.Date dataCoordenador = new java.util.Date(dataCoordenadorSQL.getTime());
         coordenador.setDataNascimento(dataCoordenador);
         
-        Curso curso = new Curso();
+      
         curso.setCursoID(resultado.getInt("idCurso"));
         curso.setAnos(resultado.getInt("anoCurso"));
         curso.setNome(resultado.getString("nomeCurso"));
         curso.setPeriodo(resultado.getInt("periodoCurso"));
         curso.setCursoAtivo(resultado.getBoolean("cursoAtivo"));
         
-        Diretor diretor = new Diretor();
+        
         diretor.setNome(resultado.getString("nomeDiretor"));
         diretor.setMatricula(resultado.getString("matriculaDiretor"));
         diretor.setEmail(resultado.getString("emailDiretor"));
@@ -97,63 +108,40 @@ public class AlunoDAO extends GeralDAO {
         java.util.Date dataDiretor = new java.util.Date(dataDiretorSQL.getTime());
         diretor.setDataNascimento(dataDiretor);
         
-        Disciplina disciplina = new Disciplina();
+        
         disciplina.setId(resultado.getInt("idDisciplina"));
         disciplina.setNome(resultado.getString("nomeDisciplina"));
         disciplina.setAtiva(resultado.getBoolean("disciplinaAtiva"));
         
-        Ifrn ifrn = new Ifrn();
+        
         ifrn.setIfrnID(resultado.getInt("idIfrn"));
         ifrn.setRazaoSocial(resultado.getString("ifrnRazaoSocial"));
         
-        ModalidadeCurso modalidadeCurso = new ModalidadeCurso();
+       
         modalidadeCurso.setModalidadeID(resultado.getInt("idModalidade"));
         modalidadeCurso.setNivel(resultado.getString("modalidadeCurso"));
         modalidadeCurso.setNome(resultado.getString("nomeModalidade"));
         
-        Permissao permissao = new Permissao();
+       
         permissao.setPermissaoID(resultado.getInt("idPermissao"));
         permissao.setNome(resultado.getString("nomePermissao"));
-               
-        Professor professor = new Professor();
-        professor.setNome(resultado.getString("nomeProfessor"));
-        professor.setMatricula(resultado.getString("matriculaProfessor"));
-        professor.setEmail(resultado.getString("emailProfessor"));
-        professor.setTelefone(resultado.getString("telefoneProfessor"));
-        professor.setCelular(resultado.getString("celularProfessor"));
-        java.sql.Date dataProfessorSQL = resultado.getDate("datanascimentoProfessor");
-        java.util.Date dataProfessor = new java.util.Date(dataProfessorSQL.getTime());
-        professor.setDataNascimento(dataDiretor);
-        
-        TecnicoAdministrativo tecAdministrativo = new TecnicoAdministrativo();
-        tecAdministrativo.setNome(resultado.getString("nomeTecAdministrativo"));
-        tecAdministrativo.setMatricula(resultado.getString("matriculaTecAdministrativo"));
-        tecAdministrativo.setEmail(resultado.getString("emailTecAdministrativo"));
-        tecAdministrativo.setTelefone(resultado.getString("telefoneTecAdministrativo"));
-        tecAdministrativo.setCelular(resultado.getString("celularTecAdministrativo"));
-        java.sql.Date dataTecAdministrativoSQL = resultado.getDate("datanascimentoTecAdministrativo");
-        java.util.Date dataTecAdministrativo = new java.util.Date(dataTecAdministrativoSQL.getTime());
-        tecAdministrativo.setDataNascimento(dataTecAdministrativo);
-               
-        TipoAtividade tipoAtividade = new TipoAtividade();
-        tipoAtividade.setId(resultado.getInt("idTipoAtividade"));
-        tipoAtividade.setNome(resultado.getString("nomeTipoAtividade"));
-        
-        TipoDocumento tipoDocumento = new TipoDocumento();
-        tipoDocumento.setId(resultado.getInt("idTipoDocumento"));
-        tipoDocumento.setNome(resultado.getString("nomeTipoDocumento"));
-        
-        TipoRequerimento tipoRequerimento = new TipoRequerimento();
-        tipoRequerimento.setId(resultado.getInt("idTipoRequerimento"));
-        tipoRequerimento.setNome(resultado.getString("nomeTipoRequerimento"));
-               
-        Turma turma = new Turma();
+         
         turma.setCodigo(resultado.getInt("codigoTurma"));
-               
-        Turno turno = new Turno();
+        
+       
         turno.setId(resultado.getInt("idTurno"));
         turno.setNome(resultado.getString("nomeTurno"));
-                                         
+        //Encapuslando objetos
+        campus.setDiretor(diretor);
+        curso.setTurno(turno);
+        curso.setCampus(campus);
+        curso.setModalidade(modalidadeCurso);
+        curso.setCoordenador(coordenador);
+        turma.setCurso(curso);
+        aluno.setCurso(curso);
+        aluno.setTurma(turma);
+        aluno.setPermissao(permissao);
+                
         
         return aluno;
     }
