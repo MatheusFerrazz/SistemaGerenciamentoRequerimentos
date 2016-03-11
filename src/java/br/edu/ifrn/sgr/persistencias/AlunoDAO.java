b3b74fa232e4b69263eb686da73136b74021d12b
Luan Medeiros Macena <luanxmedeiros@live.com>
2016-03-11 19:23:17 -0300

    Correção DAO aluno.

--- src/java/br/edu/ifrn/sgr/persistencias/AlunoDAO.java
+++ src/java/br/edu/ifrn/sgr/persistencias/AlunoDAO.java
@@ -84,56 +84,57 @@ public class AlunoDAO extends GeralDAO {
         campus.setCep(resultado.getString("cam_cep"));
         campus.setTelefone(resultado.getString("cam_telefone"));
         
-        
-        coordenador.setNome(resultado.getString("nomeCoordenador"));
-        coordenador.setMatricula(resultado.getString("matriculaCoordenador"));
-        coordenador.setEmail(resultado.getString("emailCoordenador"));
-        coordenador.setTelefone(resultado.getString("telefoneCoordenador"));
-        coordenador.setCelular(resultado.getString("celularCoordenador"));
-        java.sql.Date dataCoordenadorSQL = resultado.getDate("datanascimentoCoordenador");
+        ResultSet consultaCoordenador = executarConsulta(EnuConsultasCoordenador.SELECT_INFORMAÇÕES_COORDENADOR.toString(), resultado.getString("coo_id_PK"));
+        coordenador.setNome(consultaCoordenador.getString("pes_nome"));
+        coordenador.setMatricula(consultaCoordenador.getString("pes_matricula"));
+        coordenador.setEmail(consultaCoordenador.getString("pes_email"));
+        coordenador.setTelefone(consultaCoordenador.getString("pes_telefone"));
+        coordenador.setCelular(consultaCoordenador.getString("pes_celular"));
+        java.sql.Date dataCoordenadorSQL = consultaCoordenador.getDate("pes_data_nascimento");
         java.util.Date dataCoordenador = new java.util.Date(dataCoordenadorSQL.getTime());
         coordenador.setDataNascimento(dataCoordenador);
         
       
-        curso.setCursoID(resultado.getInt("idCurso"));
-        curso.setAnos(resultado.getInt("anoCurso"));
-        curso.setNome(resultado.getString("nomeCurso"));
-        curso.setPeriodo(resultado.getInt("periodoCurso"));
-        curso.setCursoAtivo(resultado.getBoolean("cursoAtivo"));
+        curso.setCursoID(resultado.getInt("cur_id"));
+        curso.setAnos(resultado.getInt("cur_anos"));
+        curso.setNome(resultado.getString("cur_nome"));
+        curso.setPeriodo(resultado.getInt("cur_periodos"));
+        curso.setCursoAtivo(resultado.getBoolean("cur_ativo"));
         
-        
-        diretor.setNome(resultado.getString("nomeDiretor"));
-        diretor.setMatricula(resultado.getString("matriculaDiretor"));
-        diretor.setEmail(resultado.getString("emailDiretor"));
-        diretor.setTelefone(resultado.getString("telefoneDiretor"));
-        diretor.setCelular(resultado.getString("celularDiretor"));
-        java.sql.Date dataDiretorSQL = resultado.getDate("datanascimentoDiretor");
+        ResultSet consultaDiretor = executarConsulta(EnuConsultasDiretor.SELECT_INFORMAÇÕES_DIRETOR.toString(), resultado.getString("dir_id_PK"));
+        diretor.setNome(consultaDiretor.getString("pes_nome"));
+        diretor.setMatricula(consultaDiretor.getString("pes_matricula"));
+        diretor.setEmail(consultaDiretor.getString("pes_email"));
+        diretor.setTelefone(consultaDiretor.getString("pes_telefone"));
+        diretor.setCelular(consultaDiretor.getString("pes_celular"));
+        java.sql.Date dataDiretorSQL = consultaDiretor.getDate("pes_data_nascimento");
         java.util.Date dataDiretor = new java.util.Date(dataDiretorSQL.getTime());
         diretor.setDataNascimento(dataDiretor);
         
         
-        disciplina.setId(resultado.getInt("idDisciplina"));
+       /* disciplina.setId(resultado.getInt("idDisciplina"));
         disciplina.setNome(resultado.getString("nomeDisciplina"));
-        disciplina.setAtiva(resultado.getBoolean("disciplinaAtiva"));
+        disciplina.setAtiva(resultado.getBoolean("disciplinaAtiva"));*/
         
         
+        /*   NÃO SERÁ NECESSÁRIO.
         ifrn.setIfrnID(resultado.getInt("idIfrn"));
-        ifrn.setRazaoSocial(resultado.getString("ifrnRazaoSocial"));
+        ifrn.setRazaoSocial(resultado.getString("ifrnRazaoSocial"));*/
         
        
-        modalidadeCurso.setModalidadeID(resultado.getInt("idModalidade"));
-        modalidadeCurso.setNivel(resultado.getString("modalidadeCurso"));
-        modalidadeCurso.setNome(resultado.getString("nomeModalidade"));
+        modalidadeCurso.setModalidadeID(resultado.getInt("mod_cur_id"));
+        modalidadeCurso.setNivel(resultado.getString("mod_cur_nivel"));
+        modalidadeCurso.setNome(resultado.getString("mod_cur_nome"));
         
-       
-        permissao.setPermissaoID(resultado.getInt("idPermissao"));
-        permissao.setNome(resultado.getString("nomePermissao"));
+        
+        permissao.setPermissaoID(resultado.getInt("per_id_FK"));
+        permissao.setNome(resultado.getString("per_nome"));
          
-        turma.setCodigo(resultado.getInt("codigoTurma"));
+        turma.setCodigo(resultado.getInt("trm_codigo"));
         
        
-        turno.setId(resultado.getInt("idTurno"));
-        turno.setNome(resultado.getString("nomeTurno"));
+        turno.setId(resultado.getInt("trn_id"));
+        turno.setNome(resultado.getString("trn_nome"));
         
         //Encapuslando objetos
         campus.setDiretor(diretor);
@@ -150,6 +151,11 @@ public class AlunoDAO extends GeralDAO {
         ResultSet consulta;
         GeralDAO geralDAO = new GeralDAO();
         
+        ResultSet consultaDisciplinas = executarConsulta(EnuConsultasCurso.SELECT_TODAS_DISCIPLINAS_CURSO.toString(), resultado.getString("cur_id_FK"));
+        while(consultaDisciplinas.next())
+        {
+            curso.getDisciplinas().add(new Disciplina(consultaDisciplinas.getInt("dis_id"), consultaDisciplinas.getInt("cur_id_FK"), consultaDisciplinas.getBoolean("dis_ativo"), SELECT_ALUNOS));
+        }
        /* consulta = geralDAO.executarConsulta(EnuConsultasAluno.SELECT_TODAS_DISCIPLINAS_CURSO.toString(), 1);                
         while(consulta.next())
         {
